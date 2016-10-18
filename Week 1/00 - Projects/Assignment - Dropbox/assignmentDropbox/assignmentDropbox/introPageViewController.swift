9353fce4da55c651056f5fd23507194ba6f3a670
Aaron Bailey <aaron@earnest.com>
2016-10-17 22:13:03 -0700

    Added Real Page Indicator

--- Week 1/00 - Projects/Assignment - Dropbox/assignmentDropbox/assignmentDropbox/introPageViewController.swift
+++ Week 1/00 - Projects/Assignment - Dropbox/assignmentDropbox/assignmentDropbox/introPageViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class introPageViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
@@ -26,7 +27,15 @@ class introPageViewController: UIViewController, UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        // Get the current page based on the scroll offset
+        let page : Int = Int(round(scrollView.contentOffset.x / 320))
+        
+        // Set the current page, so the dots will update
+        pageControl.currentPage = page
+    }
+    
+    
     /*
     // MARK: - Navigation
 
