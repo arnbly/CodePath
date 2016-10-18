a8be83bca95f24cb4edff25d9e7be555640f97be
Aaron Bailey <aaron@earnest.com>
2016-10-17 22:33:05 -0700

    Added Terms Web View: Milestone 12

--- Week 1/00 - Projects/Assignment - Dropbox/assignmentDropbox/assignmentDropbox/webViewController.swift
+++ Week 1/00 - Projects/Assignment - Dropbox/assignmentDropbox/assignmentDropbox/webViewController.swift
@@ -1,21 +1,29 @@
 //
-//  CreateAccountViewController.swift
+//  webViewController.swift
 //  assignmentDropbox
 //
-//  Created by Aaron Bailey on 10/16/16.
+//  Created by Aaron Bailey on 10/17/16.
 //  Copyright © 2016 Aaron Bailey. All rights reserved.
 //
 
 import UIKit
 
-class CreateAccountViewController: UIViewController {
+class webViewController: UIViewController {
 
-    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+    let url = "https://www.dropbox.com/terms?mobile=1"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
+        
+        // Convert the url String to a NSURL object.
+        let requestURL = URL(string:url)
+        // Place the URL in a URL Request.
+        let request = URLRequest(url: requestURL!)
+        // Load Request into WebView.
+        webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,13 +31,10 @@ class CreateAccountViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func backButton(_ sender: UIButton) {
-        navigationController!.popViewController(animated: true)
+    @IBAction func doneButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func didTap(_ sender: AnyObject) {
-        view.endEditing(true)
-    }
 
     /*
     // MARK: - Navigation
