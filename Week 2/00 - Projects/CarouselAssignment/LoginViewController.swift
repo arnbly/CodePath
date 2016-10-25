82bb83fe79e434ddde55dcb4c8e55e398e8518f0
Aaron Bailey <aaron@earnest.com>
2016-10-24 20:20:59 -0700

    Carousel Assignment: Milestone 6 (no 4-5)

--- Week 2/00 - Projects/CarouselAssignment/LoginViewController.swift
+++ Week 2/00 - Projects/CarouselAssignment/LoginViewController.swift
@@ -12,13 +12,35 @@ class LoginViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var loginScrollView: UIScrollView!
     
+    @IBOutlet weak var buttonParentView: UIView!
+    
+    var buttonInitialY: CGFloat!
+    var buttonOffset: CGFloat!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Load and Code
+        // Scrollview Init
         loginScrollView.delegate = self
         loginScrollView.contentSize = loginScrollView.frame.size
         loginScrollView.contentInset.bottom = 100
+        
+        //Button Offset Init
+        buttonInitialY = buttonParentView.frame.origin.y
+        buttonOffset = -120
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name.UIKeyboardWillShow, object: nil, queue: OperationQueue.main) { (notification: Notification) in
+            
+            self.buttonParentView.frame.origin.y = self.buttonInitialY + self.buttonOffset
+            
+            self.loginScrollView.contentOffset.y = self.loginScrollView.contentInset.bottom
+        }
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name.UIKeyboardWillHide, object: nil, queue: OperationQueue.main) { (notification: Notification) in
+            // Any code you put in here will be called when the keyboard is about to hide
+        }
 
 
     }
