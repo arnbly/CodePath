0efd35f1dbbcbc3c5687e0849518545e45da72d3
Aaron Bailey <aaron@earnest.com>
2016-10-24 20:50:02 -0700

    Carousel Assignment: Milestone 7

--- Week 2/00 - Projects/CarouselAssignment/LoginViewController.swift
+++ Week 2/00 - Projects/CarouselAssignment/LoginViewController.swift
@@ -13,6 +13,9 @@ class LoginViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var loginScrollView: UIScrollView!
     
     @IBOutlet weak var buttonParentView: UIView!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var buttonInitialY: CGFloat!
     var buttonOffset: CGFloat!
@@ -48,6 +51,59 @@ class LoginViewController: UIViewController, UIScrollViewDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func didPressSignIn(_ sender: UIButton) {
+        
+        //Check if fields are empty
+        if emailField.text!.isEmpty || passwordField.text!.isEmpty {
+            
+            //Init Alert Controller
+            let alertController = UIAlertController(title: "Email Required", message: "Please Enter Your Email Address", preferredStyle: .alert)
+            
+            //Create Buttons for Alert Controller
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            }
+            //Attach Buttons to Alert Controller
+            alertController.addAction(OKAction)
+            
+            //Display Alert Controller
+            present(alertController, animated: true) {
+            }
+        //If Fields not empty check correct credentials
+        } else {
+            activityIndicator.startAnimating()
+            
+            //Faux server call
+            delay(2, closure: {
+                self.activityIndicator.stopAnimating()
+                
+                //Validate Creds
+                if self.emailField.text == "email" && self.passwordField.text == "password" {
+                    
+                    //Perform Segue 
+                    self.performSegue(withIdentifier: "signInSegue", sender: nil)
+                }
+                //If Creds Incorrect display alert
+                else {
+                    
+                    //Init Alert Controller
+                    let alertController = UIAlertController(title: "Incorrect Email or Password", message: "Please Enter A Valid Email or Password", preferredStyle: .alert)
+                    
+                    //Create Buttons for Alert Controller
+                    let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                    }
+                    
+                    //Add Buttons to Alert Controller
+                    alertController.addAction(OKAction)
+                    
+                    //Show Alert Controller
+                    self.present(alertController, animated: true) {
+                    }
+                }
+            })
+        }
     }
     
 
