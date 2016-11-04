ddc708fbb7d369faf6999550d24bcad4deeddfd3
Aaron Bailey <aaron@earnest.com>
2016-11-03 22:26:09 -0700

    Tab Bar Assignment: 2

--- Week 4/00 - Projects/YouTube2/YouTube2/ViewController.swift
+++ Week 4/00 - Projects/YouTube2/YouTube2/ViewController.swift
@@ -13,11 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var feedView: UIView!
     
+    var feedOriginalCenter: CGPoint!
+    var feedOffset: CGFloat!
+    var feedClosed: CGPoint!
+    var feedOpen: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var main = UIStoryboard(name: "Main", bundle: nil)
+        
+        feedOffset = 265
+        feedClosed = feedView.center
+        feedOpen = CGPoint(x: feedView.center.x + feedOffset ,y: feedView.center.y)
         
         let menuViewController = main.instantiateViewController(withIdentifier: "MenuViewController")
         let feedViewController = main.instantiateViewController(withIdentifier: "FeedViewController")
@@ -37,12 +45,46 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didPanFeed(_ sender: UIPanGestureRecognizer) {
-                
+        //Capture Translation in variable
         let translation = sender.translation(in: view)
         
-        feedView.transform = feedView.transform.translatedBy(x: translation.x, y: 0)
+        if sender.state == .began {
+            feedOriginalCenter = feedView.center
+            
+        } else if sender.state == .changed {
+            //Move Object
+            feedView.center = CGPoint(x: feedOriginalCenter.x + translation.x, y: feedOriginalCenter.y)
+            
+            //Reset Translation to 0
+            //sender.setTranslation(CGPoint.zero, in: view)
+        } else if sender.state == .ended {
+            let velocity = sender.velocity(in: view)
+            
+            if velocity.x > 0 {
+                // Moving Right
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.feedView.center = self.feedOpen
+                }, completion: nil)
+
+                
+            } else {
+                // Moving Left
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.feedView.center = self.feedClosed
+                }, completion: nil)
+                
+                
+            }
+            
+            
+        } else {
+            print("invalid pan state")
+        }
         
-        sender.setTranslation(CGPoint.zero, in: view)
+        
+        
     }
 
 }
